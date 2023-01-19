08c2b9ff3731e6ffd6dc27d975159ac13ce06a93
Code-Holick <procoder111.abir@gmail.com>
2023-01-19 19:36:00 +0600

    checking vercel domain

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -40,7 +40,7 @@ const Checkout = () => {
   useEffect(() => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("https://${process.env.VERCEL_URL}/create-payment-intent", {
+    fetch("https://fyp-shop.vercel.app/checkout", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
